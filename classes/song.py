4eed51ff6a7fb0c56b3b8bb214354b4e612b2fa0
Costicus <yaremenko.konstantin@gmail.com>
2022-12-05 15:09:24 +0000

    last commit

---
+++ classes/song.py
@@ -0,0 +1,16 @@
+class Song:
+    def __init__(self, track_name, artist, album):
+        self.track_name = track_name
+        self.artist = artist
+        self.album = album
+    
+
+
+
+    class Song:
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+
+    def equals(self, song):
+        return song.title == self.title and song.artist == self.artist
